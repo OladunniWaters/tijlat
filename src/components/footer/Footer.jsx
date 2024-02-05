b04f4f53422e0001374173f9f0923b652d265912
oladunniwaters <waters.oladunni@gmail.com>
2024-02-05 11:21:52 +0100

    still 90% done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
       <footer className='footer'>
          <p className='footer_copyrights'>Tijlat Design ⓒ 2024 All Rights Reserved</p>
-         <p className='footer_developer'>Designed & Developed by <a href='https://github.com/OladunniWaters' className='footer_link'>Waters</a></p>
+         <p className='footer_developer'>Designed & Developed by <a href='https://github.com/OladunniWaters' target='_blank'  className='footer_link'>Waters</a></p>
       </footer>
   )
 }
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
      Explore a world where craftsmanship meets creativity, and let me guide you on a journey to discover a wardrobe that not only reflects your style but sets you apart. Elevate your fashion experience – because exceptional style deserves an extraordinary touch.
            </p>
            <div className='hero_content_button_cont'>
-              <a href='#contact' className='contact_whatsapp_link'>Contact Us</a>
+               <a href='https://wa.link/1gk4cs' target='_blank' className='contact_whatsapp_link'>Contact Us</a>
            </div>
         </div>
         
